bd3c9d9d54ce297e0468ee18c249d25409106f77
yogi fernanda <moh.yogi10065@gmail.com>
2021-06-07 19:02:05 +0700

    fixing subscriber

--- mqtt /publisher.js
+++ mqtt /publisher.js
@@ -3,33 +3,54 @@ const client = mqtt.connect("mqtt://localhost:1883")
 
 console.log("Connected flag " + client.connected);
 
-const message = {
+const pesan = {
     deviceName : "Greenhouse1",
-    Temperature: Math.floor(Math.random() * 100),
-    Humidinity: Math.floor(Math.random() * 100),
-    SoilMoinsture: Math.floor(Math.random() * 100),
-    LightSensor: Math.floor(Math.random() * 100),
-    Date : new Date()
+    Temperature: 0,
+    Humidinity: 0,
+    SoilMoinsture: 0,
+    LightSensor: 0,
+    
 }
+console.log(pesan);
 
 client.on('message', (topic, message, packet) => {
-    console.log("message is " + message);
-    console.log("topic is  " + topic);
+    //console.log("message is " + message);
+    //console.log("topic is  " + topic);
+    if (topic === "DHT1"){
+        pesan.Temperature = message.toString()
+        console.log(`pesan ${pesan}`);
+     }
+    else if (topic == "DHT2"){
+        pesan.Humidinity = message.toString()
+    }
+    else if (topic == "DHT3"){
+        pesan.LightSensor = message.toString()
+    }
+    else if (topic == "DHT4"){
+        pesan.SoilMoinsture = message.toString()
+        console.log(`pesan ${pesan}`);
+    }
+    console.log(message.toString);
+    
 });
+
 
 client.on('connect', ()=>{
     setInterval(function(){
-        client.publish("/deviceInfo", JSON.stringify(message));
+        client.publish("/deviceInfo", JSON.stringify(pesan));
         console.log('Pesan terkirim');
-        console.log(JSON.stringify("IoT Decice Informatin"))
-    }, 2000);
+        console.log(pesan)
+    }, 1000);
 })
 
 client.on('connect', ()=>{
-    client.subscribe('deviceInfo');
+    client.subscribe('DHT1');
+    client.subscribe('DHT2');
+    client.subscribe('DHT3');
+    client.subscribe('DHT4');
 });
 
-client.on('message', (topic, message)=>{
-    message = JSON.parse(message.toString())
+client.on('message', ( message)=>{
+    message = message.toString()
     console.log(message)
 })
